c2ed0fd19e1db30db8c7aace45b76ae0246bb5c2
eva montgomery <eva.m.montgomery@gmail.com>
2019-12-06 17:40:48 -0500

    adds finished DOM link-exercise

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,7 @@
+const engineLinks = document.querySelector(".js-link-container");
+const googleLink = document.querySelector(".js-google");
+const bingLink = document.querySelector(".js-bing");
+const duckLink = document.querySelector(".js-duck");
 
 
 const addresses = [
@@ -5,4 +9,53 @@ const addresses = [
     "https://bing.com",
     "https://duckduckgo.com"
 ];
+
+
+function linkWithPath(linkPath) {
+    const a = document.createElement("a");
+    a.href = linkPath;
+    return a;
+}
+
+
+function displayOne(path) {
+    console.log(path);
+    const a = linkWithPath(path);
+    console.log(a);
+    appendLinkToButton(a);
+}
+
+function searchEngines(linkArray) {
+    console.log(linkArray);
+    for (let path of linkArray) {
+        console.log(path);
+        createLink(path);
+    }
+displayOne(addresses);
+
+}
+
+function appendLinkToButton(linkEL) {
+    js-link-container.appendChild(linkEL);
+}
+appendLinkToButton(a);
+
+engineLinks.addEventListener("click", searchEngines);
+
+
+//function.append
+
+// searchEngines.addEventListener();
+
+// function showButton(aLink, linkPath) {
+//     aLink.src = linkPath;
+// }
+
+// function showGoogle() {
+//     console.log(engineLinks, addresses);
+// }
+
+
+
+
 
